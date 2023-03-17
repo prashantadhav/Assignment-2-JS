0e9611cacd9e2000639e1eb108d3ad3a7f111bfa
Prashant <95129447+prashantadhav@users.noreply.github.com>
2023-03-17 23:35:27 +0530

    Assignment 2

---
+++ assignment2.js
@@ -0,0 +1,9 @@
+// Define a function called "multiplyByTwo" that takes one parameter: "num"
+function multiplyByTwo(num) {
+    // Multiply the input number by two and return the result
+    return num * 2;
+  }
+  
+  // Test the function by calling it with a sample input
+  console.log(multiplyByTwo(5)); // Output: 10
+  
